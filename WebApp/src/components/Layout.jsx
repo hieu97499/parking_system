274f85270bb46c32dc52ba883e2e349347c36ae0
hieu97499 <hieu97499@st.vimaru.edu.vn>
2026-05-29 19:08:14 +0700

    fix: prevent iOS auto-zoom on inputs + overflow-x hidden + viewport-fit cover

--- WebApp/src/components/Layout.jsx
+++ WebApp/src/components/Layout.jsx
@@ -4,9 +4,9 @@ import BottomNav from './BottomNav';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col max-w-md mx-auto bg-white shadow-xl relative">
+    <div className="min-h-dvh flex flex-col max-w-md mx-auto bg-white shadow-xl relative overflow-x-hidden">
       <Header />
-      <main className="flex-1 overflow-y-auto pb-20 bg-slate-50">
+      <main className="flex-1 overflow-y-auto overflow-x-hidden pb-20 bg-slate-50">
         <Outlet />
       </main>
       <BottomNav />
